6884055546ebb85962874356e2baaaaa4b1321f7
Empirial <mphelalufuno1.0@gmail.com>
2025-06-12 22:51:21 +0200

    Added frontend and updated backend files

--- backend/src/index.js
+++ backend/src/index.js
@@ -44,9 +44,9 @@ app.post("/api/paypal/webhook", async (req, res) => {
     if (amount === 2.00 && currency === "USD" && email) {
       const { createClient } = require('@supabase/supabase-js');
 
-    const SUPABASE_URL = "https://fkuegctjylijbjnzmawl.supabase.co";
-    const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZrdWVnY3RqeWxpamJqbnptYXdsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0ODE5OTQzNiwiZXhwIjoyMDYzNzc1NDM2fQ.no2Ddi9kgH6gMZv6FMSuBD6su3IKyOPqaFmbbP-Z1o4";
-    const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+      const SUPABASE_URL = "https://fkuegctjylijbjnzmawl.supabase.co";
+      const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZrdWVnY3RqeWxpamJqbnptYXdsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0ODE5OTQzNiwiZXhwIjoyMDYzNzc1NDM2fQ.no2Ddi9kgH6gMZv6FMSuBD6su3IKyOPqaFmbbP-Z1o4";
+      const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
       module.exports = supabase;
       const { data: licenseKeys, error } = await supabase
